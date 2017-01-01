9993c4489598ca9a2ec21e5a7ef7911b894cc396
Kenneth Aladi <kennethmichael11@gmail.com>
2017-01-01 01:53:58 +0100

    made the up button down clickable

--- main.js
+++ main.js
@@ -18,8 +18,11 @@ $(document).ready(function() {
     );
   });
 
-  $(".menu-toggler").on("click", function() {
-    $(this).toggleClass("open");
-    $(".top-nav").toggleClass("open");
-  });
+  $("#up").on("click", function() {
+    $("html, body").animate(
+      {
+        scrollTop: 0
+      },
+      2000
+    );
 });
